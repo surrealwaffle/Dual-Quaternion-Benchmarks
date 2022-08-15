073907fc08c4fcba4f2f362b2752014c35d1f07c
surrealwaffle <aomaniac@gmail.com>
2022-08-14 19:56:03 -0700

    fixed and updated config.hpp

--- config.hpp
+++ config.hpp
@@ -3,27 +3,32 @@
 // Define PASS_QUATERNIONS_BY_REFERENCE to make quaternions passed by reference 
 // (defaults to pass-by-value).
 #ifdef PASS_QUATERNIONS_BY_REFERENCE
-template<typename T> using qarg = T&;
+#  define QUATERNION_PARAM_REFERENCE &
 #else
-template<typename T> using qarg = T;
+#  define QUATERNION_PARAM_REFERENCE 
 #endif // PASS_QUATERNIONS_BY_REFERENCE
 
 // Define PASS_VECTORS_BY_REFERENCE to make vectors passed by reference 
 // (defaults to pass-by-value).
 #ifdef PASS_VECTORS_BY_REFERENCE
-template<typename T> using vecarg = T&;
+#  define VECTOR_PARAM_REFERENCE &
 #else
-template<typename T> using vecarg = T;
+#  define VECTOR_PARAM_REFERENCE 
 #endif // PASS_QUATERNIONS_BY_REFERENCE
 
 // Define PASS_DUAL_QUATERNIONS_BY_REFERENCE to make dual quaternions passed by 
 // reference (defaults to pass-by-value).
 #ifdef PASS_DUAL_QUATERNIONS_BY_REFERENCE
-template<typename T> using dqarg = T&;
+#  define DUAL_QUATERNION_PARAM_REFERENCE &
 #else
-template<typename T> using dqarg = T;
-#endif // PASS_DUAL_QUATERNIONS_BY_REFERENCE
+#  define DUAL_QUATERNION_PARAM_REFERENCE 
+#endif // PASS_QUATERNIONS_BY_REFERENCE
 
-template<typename T> using cqarg   = qarg<const T>;
+template<typename T> using qarg  = T QUATERNION_PARAM_REFERENCE;
+template<typename T> using cqarg = qarg<const T>;
+
+template<typename T> using vecarg  = T VECTOR_PARAM_REFERENCE;
 template<typename T> using cvecarg = vecarg<const T>;
-template<typename T> using cdqarg  = dqarg<const T>;
+
+template<typename T> using dqarg  = T DUAL_QUATERNION_PARAM_REFERENCE;
+template<typename T> using cdqarg = dqarg<const T>;
