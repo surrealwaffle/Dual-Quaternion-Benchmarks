c26a1c00b48639b02ff65c0cd2619be9a2dde72e
surrealwaffle <aomaniac@gmail.com>
2022-08-14 18:36:21 -0700

    added more configuration options for config.hpp

--- config.hpp
+++ config.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
-// The macro PASS_QUATERNIONS_BY_REFERENCE causes quaternions and dual quaternions 
-// to be passed by (const) reference rather than (const) value to functions.
+// Define PASS_QUATERNIONS_BY_REFERENCE to make quaternions passed by reference 
+// (defaults to pass-by-value).
 #ifdef PASS_QUATERNIONS_BY_REFERENCE
 template<typename T> using qarg = T&;
 #else
 template<typename T> using qarg = T;
 #endif // PASS_QUATERNIONS_BY_REFERENCE
 
-template<typename T> using cqarg = qarg<const T>;
+// Define PASS_VECTORS_BY_REFERENCE to make vectors passed by reference 
+// (defaults to pass-by-value).
+#ifdef PASS_VECTORS_BY_REFERENCE
+template<typename T> using vecarg = T&;
+#else
+template<typename T> using vecarg = T;
+#endif // PASS_QUATERNIONS_BY_REFERENCE
+
+// Define PASS_DUAL_QUATERNIONS_BY_REFERENCE to make dual quaternions passed by 
+// reference (defaults to pass-by-value).
+#ifdef PASS_DUAL_QUATERNIONS_BY_REFERENCE
+template<typename T> using dqarg = T&;
+#else
+template<typename T> using dqarg = T;
+#endif // PASS_DUAL_QUATERNIONS_BY_REFERENCE
+
+template<typename T> using cqarg   = qarg<const T>;
+template<typename T> using cvecarg = vecarg<const T>;
+template<typename T> using cdqarg  = dqarg<const T>;
